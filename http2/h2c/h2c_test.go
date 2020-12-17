ee25f2ce9bee01b932ad1ffb3d701e2bdc570de3
Luke Shumaker <lukeshu@datawire.io>
2020-12-16 18:20:05 -0700

    wip

--- http2/h2c/h2c_test.go
+++ http2/h2c/h2c_test.go
@@ -17,6 +17,7 @@ import (
 	"net/http/httptest"
 	"sync"
 	"testing"
+	"time"
 
 	"golang.org/x/net/http2"
 )
@@ -147,5 +148,89 @@ func TestShutdownIdle(t *testing.T) {
 	// At this point, the workers should have been all cleaned up,
 	// so workers.Wait() should return.  This will hang ServeConn
 	// isn't getting shut down correctly.
+	workers.Wait()
+}
+
+func TestShutdownActive(t *testing.T) {
+	h1requestReceived := make(chan struct{})
+	h2requestReceived := make(chan struct{})
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 1 {
+			close(h1requestReceived)
+		} else {
+			close(h2requestReceived)
+		}
+		t.Logf("Received HTTP/%v request", r.ProtoMajor)
+		<-r.Context().Done()
+		t.Logf("Canceled HTTP/%v request", r.ProtoMajor)
+	})
+
+	h2s := &http2.Server{}
+	// Wrap the 'NewHandler' so that we can track when it cleans up.
+	var workers sync.WaitGroup
+	h1s := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		workers.Add(1)
+		defer workers.Done()
+		NewHandler(handler, h2s).ServeHTTP(w, r)
+	}))
+
+	h1s.Start()
+	defer h1s.Close()
+
+	// Launch two requests: one HTTP/2, and one HTTP/1. We really
+	// only need to test the HTTP/2 case, but I like the symmetry
+	// of showing that they both behave the same.
+	//
+	// They should both hang until the server is forcefully
+	// Close()ed.
+	workers.Add(2)
+	go func() {
+		defer workers.Done()
+		client := &http.Client{
+			Transport: &http2.Transport{
+				AllowHTTP: true,
+				DialTLS: func(network, addr string, _ *tls.Config) (net.Conn, error) {
+					return net.Dial(network, addr)
+				},
+			},
+		}
+		resp, err := client.Get(h1s.URL)
+		if err == nil {
+			t.Error("HTTP/2 client expected an error")
+		}
+		if resp != nil {
+			t.Errorf("HTTP/2 client expected no response, but got: %v", resp)
+		}
+	}()
+	go func() {
+		defer workers.Done()
+		client := http.DefaultClient
+		resp, err := client.Get(h1s.URL)
+		if err == nil {
+			t.Error("HTTP/1 client expected an error")
+		}
+		if resp != nil {
+			t.Errorf("HTTP/1 client expected no response, but got: %v", resp)
+		}
+	}()
+
+	<-h1requestReceived
+	<-h2requestReceived
+	// Because both requests hang until the r.Context() is
+	// canceled, the Shutdown should hang for the full 5 seconds,
+	// then return context.DeadlineExceeded.
+	t.Log("Shutdown...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := h1s.Config.Shutdown(ctx)
+	if err != context.DeadlineExceeded {
+		t.Errorf("expected error %v, but got %v", context.DeadlineExceeded, err)
+	}
+	// This should both cause the 2 clients to return with an
+	// error, and cause the r.Context()s to be canceled, causing
+	// the handlers to return.
+	t.Log("Close...")
+	h1s.Config.Close()
 	workers.Wait()
 }
